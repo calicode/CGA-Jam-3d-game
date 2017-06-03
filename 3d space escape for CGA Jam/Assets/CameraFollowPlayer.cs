1e3a6b9e5170c590dd76bd15be37fb09d6e32328
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-06-02 22:04:15 -0500

    Basic movement works

---
+++ 3d space escape for CGA Jam/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public GameObject playerObject;
+    private Vector3 offset;
+    public float cameraOffset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - playerObject.transform.position;
+        cameraOffset = 15f;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //float newX = (playerObject.transform.position.x + offset.x) - cameraOffset;
+        transform.position = new Vector3(playerObject.transform.position.x + cameraOffset, transform.position.y, transform.position.z);
+    }
+}
---
+++ 3d space escape for CGA Jam/Assets/ShipController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float baseMoveSpeed = 1;
+    public float currentMoveSpeed;
+    public float speedIncreaseIncrement = 1f;
+    public float maxMoveSpeed = 20f;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentMoveSpeed = baseMoveSpeed;
+        InvokeRepeating("IncreaseSpeed", 1, 1);
+    }
+
+
+    void IncreaseSpeed()
+    {
+
+        if (currentMoveSpeed <= maxMoveSpeed)
+        {
+            currentMoveSpeed += speedIncreaseIncrement;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayerForward();
+    }
+    void MovePlayerForward()
+    {
+
+        transform.position = new Vector3(transform.position.x - (Time.deltaTime * currentMoveSpeed), 2, 2);
+    }
+
+
+}
